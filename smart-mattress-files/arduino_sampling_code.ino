ef59b432eed6700b6953f14db38d0ca3ef2fcca1
RoiShahar <102615216+RoiShahar@users.noreply.github.com>
2024-10-06 10:08:50 +0300

    uploading the arduino loop code file

---
+++ smart-mattress-files/arduino_sampling_code.ino
@@ -0,0 +1,24 @@
+// the Matrix indexes:
+// [11 12 21 22]
+// [13 14 23 24]
+// [31 32 41 42]
+// [33 34 43 44]
+
+void setup() {
+  // Begin serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int pin = A0; pin <= A15; pin++) {
+    // Read the value from the pin
+    int sensorValue = analogRead(pin);
+    Serial.println(sensorValue);
+  }
+  
+  // separator for easier parsing
+  Serial.println("---");
+
+  // delay for each data message
+  delay(500);
+}
